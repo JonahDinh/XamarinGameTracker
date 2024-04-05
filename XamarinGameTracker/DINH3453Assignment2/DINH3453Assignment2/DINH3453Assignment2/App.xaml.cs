a003602c68c77ec3885fafc7813f300e414156ab
Jonah Dinh <94314036+JonahDinh@users.noreply.github.com>
2024-04-05 16:47:21 -0400

    Add files via upload

---
+++ XamarinGameTracker/DINH3453Assignment2/DINH3453Assignment2/DINH3453Assignment2/App.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DINH3453Assignment2
+{
+    public partial class App : Application
+    {
+        static Database database;
+
+        public static Database Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new Database(DependencyService.Get<IFileHelper>().GetLocalFilePath("DatabaseSQLite.db3"));
+                }
+                return database;
+            }
+
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new OpponentsListPage());
+
+            ToolbarItem gamesToolbarItem = new ToolbarItem { Text = "Games" };
+            gamesToolbarItem.Clicked += async (sender, e) =>
+            {
+                await MainPage.Navigation.PushAsync(new GamesListPage());
+            };
+
+            ToolbarItem settingsToolbarItem = new ToolbarItem { Text = "Settings" };
+            settingsToolbarItem.Clicked += async (sender, e) =>
+            {
+                await MainPage.Navigation.PushAsync(new SettingsPage());
+            };
+
+            MainPage.ToolbarItems.Add(gamesToolbarItem);
+            MainPage.ToolbarItems.Add(settingsToolbarItem);
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+
+    public interface IFileHelper
+    {
+        string GetLocalFilePath(string filename);
+    }
+
+}
---
+++ XamarinGameTracker/DINH3453Assignment2/DINH3453Assignment2/DINH3453Assignment2/settingsPage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace DINH3453Assignment2
+{
+    public class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            Title = "Settings";
+
+            //Button to reset
+            Button resetButton = new Button
+            {
+                Text = "Reset App",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                IsEnabled = true
+            };
+
+            resetButton.Clicked += (sender, e) =>
+            {
+                App.Database.ResetApp();
+                //Disabling the button after click
+                resetButton.IsEnabled = false;
+                
+            };
+            Content = new StackLayout
+            {
+                Children = {
+                    resetButton
+                }
+            };
+        }
+    }
+}
